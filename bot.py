d4b3ce091c1944a192ad30315a400de41bec738a
mixa1334 <m.kravcov@inbox.ru>
2022-05-12 12:01:03 +0300

    fixes

--- bot.py
+++ bot.py
@@ -13,48 +13,52 @@ def get_wiki(s):
         result = wikipedia.search(s)
         page = wikipedia.page(result[0])
         wikitext = page.content[:1000]
-        wikimas = wikitext.split('.')
-        wikimas = wikimas[:-1]
-        wikitext2 = ''
-        for x in wikimas:
-            if not ('==' in x):
-                if len((x.strip())) > 3:
-                    wikitext2 = wikitext2 + x + '.'
-            else:
-                break
-        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
-        return wikitext2
+        return split_content(wikitext)
     except Exception as e:
         return 'Нет Информации'
 
 
-@bot.message_handler(commands=["start", "help"])
+def split_content(content):
+    s = content.split('.')
+    s = s[:-1]
+    wikitext = ''
+    for x in s:
+        if not ('==' in x):
+            if len((x.strip())) > 3:
+                wikitext = wikitext + x + '.'
+        else:
+            break
+    wikitext = re.sub('\([^()]*\)', '', wikitext)
+    wikitext = re.sub('\([^()]*\)', '', wikitext)
+    wikitext = re.sub('\{[^\{\}]*\}', '', wikitext)
+    return wikitext
+
+
+@bot.message_handler(commands=["start"])
 def start(message):
     global userInput
     if not userInput:
         bot.send_message(message.chat.id,
-                         "Привет!\n- /artist получить информацию об артисте\n- /song получить информацию о песне")
+                         "Привет!\n- /артист получить информацию об артисте\n- /песня получить информацию о песне")
 
 
-@bot.message_handler(commands=["artist"])
+@bot.message_handler(commands=["артист"])
 def artist(message):
     global userInput
     if not userInput:
         userInput = True
-        bot.send_message(message.chat.id, "Введите имя артиста или напишите \'/done\' для выхода")
+        bot.send_message(message.chat.id, "Введите имя артиста или напишите \'/готово\' для выхода")
 
 
-@bot.message_handler(commands=["song"])
+@bot.message_handler(commands=["песня"])
 def song(message):
     global userInput
     if not userInput:
         userInput = True
-        bot.send_message(message.chat.id, "Введите имя песни или напишите \'/done\' для выхода")
+        bot.send_message(message.chat.id, "Введите имя песни или напишите \'/готово\' для выхода")
 
 
-@bot.message_handler(commands=["done"])
+@bot.message_handler(commands=["готово"])
 def done(message):
     global userInput
     if userInput:
